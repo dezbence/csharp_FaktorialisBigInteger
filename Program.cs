a8b9a21fb48f47f34b936bb2976b9bc191933646
dezbence <dezbence@gmail.com>
2020-09-30 11:56:03 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Numerics;
+
+namespace FaktoriálisBigInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+           
+            Console.WriteLine("Faktroiális meghatározása 20-40 között");
+            for (int szám = 0; szám <= 40; szám++)
+            {
+                BigInteger faktor = 1;
+                for (int i = 2; i <= szám; i++)
+                {
+                    faktor = faktor * i;
+                }
+                Console.WriteLine($"{szám}!={faktor}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
